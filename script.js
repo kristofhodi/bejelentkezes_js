7c86317fdf7bda4929f9ec546bbc5f31010fe9e8
Hódi Kristóf <kristof.hodi@gmail.com>
2025-09-12 10:20:53 +0200

    oldalak, script kezdes

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("bejelentkezes").addEventListener("click", () => {
+    const email = document.getElementById("emailB").value
+    const jelszo = document.getElementById("jelszoB").value
+});
