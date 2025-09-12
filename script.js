dd6cb0e56115f50e90a3e117b114805ec9a4123d
kristofhodi <kristof.hodi@gmail.com>
2025-09-12 15:11:42 +0200

    rawdog

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
+const szoveg = document.getElementById("szoveg");
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
 document.getElementById("bejelentkezes").addEventListener("click", () => {
     const email = document.getElementById("emailB").value;
     const jelszo = document.getElementById("jelszoB").value;
-    const szoveg = document.getElementById("szoveg");
-
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
     if (!email) {
-        szoveg.innerHTML = "Kérem add meg az email címedet!";
+        szoveg.innerHTML = "Kérlek add meg az email címedet!";
     } else if (!emailRegex.test(email)) {
         szoveg.innerHTML = "Kérlek valós email címet adj meg!";
     } else if (!jelszo) {
-        szoveg.innerHTML = "Kérem add meg a jelszavad!";
+        szoveg.innerHTML = "Kérlek add meg a jelszavad!";
     } else {
         szoveg.innerHTML = "A bejelentkezés sikeres volt!";
+    }
+});
+
+document.getElementById("regisztracio").addEventListener("click", () => {
+    const email = document.getElementById("emailR").value;
+    const jelszo = document.getElementById("jelszoR").value;
+    const jelszoujra = document.getElementById("jelszoRR").value;
+
+    if (!email) {
+        szoveg.innerHTML = "Kérlek add meg az email címedet!";
+    } else if (!emailRegex.test(email)) {
+        szoveg.innerHTML = "Kérlek valós email címet adj meg!";
+    } else if (!jelszo) {
+        szoveg.innerHTML = "Kérlek add meg a jelszavad!";
+    } else if (jelszo != jelszoujra) {
+        szoveg.innerHTML = "A két jelszó nem egyezik meg!"
+    }else {
+        szoveg.innerHTML = "A regisztráció sikeres volt!";
     }
 });
