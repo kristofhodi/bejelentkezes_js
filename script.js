1f3f792cf1465aa6a3ac11c1c3929a282c0ad4bd
Hódi Kristóf <kristof.hodi@gmail.com>
2025-09-12 10:36:39 +0200

    bejelentkezes almost

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 document.getElementById("bejelentkezes").addEventListener("click", () => {
-    const email = document.getElementById("emailB").value
-    const jelszo = document.getElementById("jelszoB").value
+    const email = document.getElementById("emailB").value;
+    const jelszo = document.getElementById("jelszoB").value;
+    const szoveg = document.getElementById("szoveg");
+
+    //emailt vhogy ellenorizni kell idk how
+
+    if (!jelszo) {
+        szoveg.innerHTML = "Kérem add meg a jelszavad!";
+    } else {
+        szoveg.innerHTML = "A bejelentkezés sikeres volt!";
+    }
 });
